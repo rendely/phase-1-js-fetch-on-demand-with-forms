2df4f623d02e22629fef58504ee5f342bf6479e7
matthew rendely <matthew.rendely@gmail.com>
2023-03-18 11:38:19 -0700

    done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 const init = () => {
-  
+  const inputForm = document.querySelector('form');
+  inputForm.addEventListener('submit',e =>{
+    e.preventDefault();
+    const bookId = inputForm.searchByID.value;
+    console.log('Book id:',bookId);
+    getMovieById(bookId);
+  })
+}
+
+function getMovieById(bookId){
+  fetch(`http://localhost:3000/movies/${bookId}`)
+  .then(r=>r.json())
+  .then(d => showMovieDetails(d));
+}
+
+function showMovieDetails(d){
+  const title = document.querySelector('#movieDetails > h4');
+  const summary = document.querySelector('#movieDetails > p');
+  title.textContent = d.title;
+  summary.textContent = d.summary;
 }
 
 document.addEventListener('DOMContentLoaded', init);
